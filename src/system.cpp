455c2eb000b5a63e0278ebe15b8e95528715fefa
Christoph <christoph.bodner@gmail.com>
2021-02-28 10:50:24 +0100

    add system constructor and set static sys info

--- src/system.cpp
+++ src/system.cpp
@@ -14,6 +14,11 @@ using std::size_t;
 using std::string;
 using std::vector;
 
+// We poll infos that do not change only once when we initialize the class
+System::System(): 
+    kernel_(LinuxParser::Kernel())
+    ,os_(LinuxParser::OperatingSystem()) {}
+
 // TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
@@ -21,13 +26,13 @@ Processor& System::Cpu() { return cpu_; }
 vector<Process>& System::Processes() { return processes_; }
 
 // TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return LinuxParser::Kernel(); }
+std::string System::Kernel() { return kernel_; }
 
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
 // TODO: Return the operating system name
-std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
+std::string System::OperatingSystem() { return os_; }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
