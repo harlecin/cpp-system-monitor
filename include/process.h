335989f4b3c876069e3035d4156af92b72376650
Christoph <christoph.bodner@gmail.com>
2021-03-07 20:19:00 +0100

    remove const to make it work with sort

--- include/process.h
+++ include/process.h
@@ -15,14 +15,14 @@ class Process {
   float CpuUtilization();                  // TODO: See src/process.cpp
   std::string Ram();                       // TODO: See src/process.cpp
   long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  bool operator<(Process a);             // TODO: See src/process.cpp
 
   // TODO: Declare any necessary private members
  private:
-    int const pid_;
-    std::string const uid_;
-    std::string const user_;
-    std::string const command_;
+    int pid_;
+    std::string uid_;
+    std::string user_;
+    std::string command_;
 };
 
 #endif
