9aebfc0080c4d2bd82dd15141e391a4008005570
Christoph <christoph.bodner@gmail.com>
2021-03-07 21:49:29 +0100

    remove commented code

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,5 @@
 int main() {
   System system;
   NCursesDisplay::Display(system);
-//    Processor processor;
-//    processor.Utilization();
-//    LinuxParser::Command(1);
-//    LinuxParser::Uid(1);
-//    LinuxParser::User(1);
-//    LinuxParser::Ram(1);
-//    LinuxParser::StatParser(1);
-//    Process process = Process(1);
-//    process.UpTime();
-//    process.CpuUtilization();
-
 }
+
